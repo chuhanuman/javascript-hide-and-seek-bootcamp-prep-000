be69f7cd018898721dd97b68dd2aa68f55457a7f
Hanuman Chu <chuhanuman@gmail.com>
2018-02-23 13:15:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n) {
+  var allRanked = document.querySelectorAll('ul.ranked-list li');
+  for (let i=0; i<allRanked.length;i++) {
+    allRanked[i].innerHTML = (parseInt(allRanked[i].innerHTML)+n);
+  }
+}
+
+function deepestChild(selector) {
+  console.log('RUN');
+  var all;
+  if (selector){
+    all = document.querySelector(selector);
+  } else {
+    selector = '#grand-node';
+    all = document.querySelector(selector);
+  }
+  var canWeGoDeeper = false;
+  for (let i=0;i<1;i++) {
+    console.log('LOOP');
+    if (document.querySelector(selector+" div")) {
+      canWeGoDeeper = true;
+      console.log('LOOK');
+      selector = deepestChild(selector+" div");
+      canWeGoDeeper = false;
+    } else {
+      selector = document.querySelector(selector);
+      console.log('DONE');
+    }
+  }
+  if (canWeGoDeeper === false){
+    console.log(selector);
+    return selector;
+  }
+}
+
+
+
